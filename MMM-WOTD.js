ba8736808c2a851b5671bd1b6c275996800f963c
mumblebaj <bernardmumble@gmail.com>
2022-05-08 22:33:27 +0200

    update display styling

--- MMM-WOTD.js
+++ MMM-WOTD.js
@@ -66,7 +66,7 @@ Module.register("MMM-WOTD", {
 
             const translation = document.createElement("div")
             translation.innerHTML = this.apiData[0].translation
-            translation.className = "italic medium"
+            translation.className = "bright medium"
             wrapper.appendChild(translation);
 
             if(this.config.showExamples) {
